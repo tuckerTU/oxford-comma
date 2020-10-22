566ccafe905f77f8a19c83f120601d508c53ae70
tuckerTU <tuckerishere@gmail.com>
2020-10-22 12:18:10 -0600

    commit tree 085116658b742d6220e1c1eaa429b629890d67f9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
-end
+    array = [*array]
+    case array.size
+    when 0
+      ''
+    when 1
+      array[0].to_s
+    when 2
+      array.join(' and ')
+    else
+      array_copy = array
+      array_copy[-1] = "and #{array_copy[-1]}"
+      array_copy.join(', ')
+    end
+  end
